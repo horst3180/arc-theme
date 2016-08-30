e6ec849cfe83d41a6597b26f9979587566eeefd2
Horst3180 <horst3180@users.noreply.github.com>
2016-08-31 00:02:23 +0200

    refactor xfwm theme
    -now png based
    -add borders to light theme
    -add thicker bottom border for easier resizing

---
+++ common/xfwm4/render-assets.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+INKSCAPE="/usr/bin/inkscape"
+OPTIPNG="/usr/bin/optipng"
+
+SRC_FILE="assets.svg"
+DARK_SRC_FILE="assets-dark.svg"
+ASSETS_DIR="assets"
+DARK_ASSETS_DIR="assets-dark"
+
+INDEX="assets.txt"
+
+for i in `cat $INDEX`
+do 
+if [ -f $ASSETS_DIR/$i.png ]; then
+    echo $ASSETS_DIR/$i.png exists.
+else
+    echo
+    echo Rendering $ASSETS_DIR/$i.png
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+fi
+if [ -f $DARK_ASSETS_DIR/$i.png ]; then
+    echo $DARK_ASSETS_DIR/$i.png exists.
+else
+    echo
+    echo Rendering $DARK_ASSETS_DIR/$i.png
+    $INKSCAPE --export-id=$i \
+              --export-id-only \
+              --export-png=$DARK_ASSETS_DIR/$i.png $DARK_SRC_FILE >/dev/null \
+    && $OPTIPNG -o7 --quiet $DARK_ASSETS_DIR/$i.png 
+fi
+done
+exit 0
